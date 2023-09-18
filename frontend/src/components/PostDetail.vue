21eebe82eee141d52bacfa9bc75430af0719dfe9
Lee-Siyoung <tldud4861@gmail.com>
2023-09-18 14:22:57 +0900

    post 수정기능

--- frontend/src/components/PostDetail.vue
+++ frontend/src/components/PostDetail.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="post-detail">
     <h2>{{ post.name }} 상세 내용</h2>
-    <p>{{ post.detail }}</p>
+    <p class="detail" v-if="!isEditing">{{ post.detail }}</p>
+    <div v-else>
+      <input
+        class="edit-input"
+        v-model="editedName"
+        type="text"
+        placeholder="포스트 제목"
+      />
+      <textarea
+        class="edit-textarea"
+        v-model="editedDetail"
+        placeholder="포스트 상세 내용"
+      ></textarea>
+    </div>
+    <button v-if="!isEditing" @click="startEditing">수정</button>
+    <button v-else @click="saveEdit">저장</button>
   </div>
 </template>
 
@@ -11,7 +26,10 @@ import axios from "axios";
 export default {
   data() {
     return {
-      post: {}, // post 데이터를 저장할 객체를 초기화합니다.
+      post: {}, // post 데이터를 저장할 객체를 초기화
+      isEditing: false, // 편집 모드 여부를 나타내는 변수
+      editedName: "", // 편집 중인 포스트 이름을 저장하는 변수
+      editedDetail: "", // 편집 중인 포스트 내용을 저장하는 변수
     };
   },
   methods: {
@@ -20,9 +38,32 @@ export default {
         const boardId = this.$route.params.boardId;
         const postId = this.$route.params.postId;
         const response = await axios.get(`/boards/${boardId}/post/${postId}`);
-        this.post = response.data; // 가져온 데이터를 post 객체에 할당합니다.
+        this.post = response.data; // 가져온 데이터를 post 객체에 할당
       } catch (error) {
         console.error("Error fetching post:", error);
+      }
+    },
+    startEditing() {
+      // 수정 모드 시작
+      this.isEditing = true;
+      this.editedName = this.post.name; // 편집 중인 제목 초기화
+      this.editedDetail = this.post.detail; // 편집 중인 내용 초기화
+    },
+    async saveEdit() {
+      // 수정된 내용 저장
+      try {
+        const boardId = this.$route.params.boardId;
+        const postId = this.$route.params.postId;
+        await axios.put(`/boards/${boardId}/post/${postId}`, {
+          name: this.editedName,
+          detail: this.editedDetail,
+        });
+        // 수정 완료 후 데이터를 업데이트하고 수정 모드 종료
+        this.post.name = this.editedName;
+        this.post.detail = this.editedDetail;
+        this.isEditing = false;
+      } catch (error) {
+        console.error("Error saving edit:", error);
       }
     },
   },
@@ -33,6 +74,11 @@ export default {
 </script>
 
 <style>
+.detail {
+  background-color: #ffffff;
+  border: 1px solid #b8b8b8;
+  height: 450px;
+}
 .post-detail {
   width: 600px;
   height: 80vh;
@@ -51,5 +97,28 @@ export default {
 
 .post-detail p {
   font-size: 16px;
+}
+
+.edit-input,
+.edit-textarea {
+  width: 96%;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 10px;
+  resize: vertical;
+}
+
+.edit-textarea {
+  height: 400px; /* 텍스트 입력 필드의 높이를 조절합니다. */
+}
+
+.post-detail button {
+  background-color: #696df2;
+  color: #ffffff;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
 }
 </style>
